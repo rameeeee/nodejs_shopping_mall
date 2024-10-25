3266dfeef0e59242f5b46cae36cbf2cab4deaf52
rameeeee <moderato611@naver.com>
2024-10-25 17:35:50 +0900

    [241025] 회원가입 백엔드

--- be/app.js
+++ be/app.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+const indexRouter = require('./routes/index')
 
 require('dotenv').config()
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json()) // req.body가 객체로 인식됨
+
+app.use("/api", indexRouter)
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
